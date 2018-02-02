061b1afe2bb03f7edbaa2592ecc9ab5b4e37c735
Manigandan1525 <32451747+Manigandan1525@users.noreply.github.com>
2018-02-02 18:47:59 +0530

    Add files via upload

---
+++ Hangman game project/console.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "graphics.h"
+#include<iostream>
+#include<string>
+using namespace std;
+class console
+{
+public:
+	console();
+	~console();
+	void getusername();
+	void game();
+	void gamecategory();
+	void playgame();
+	void result();
+};
+
